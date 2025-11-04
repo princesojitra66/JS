92baa5e41dec76eab8c2fd6c9ae9def4a141e2a8
princesojitra66 <princesojitra3914@gmail.com>
2025-11-04 10:41:38 +0530

    JS PART

---
+++ Student-Card/student.js
@@ -0,0 +1,409 @@
+var student1 = document.querySelector(".student1");
+let card1 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output1 ="";
+for(let key in card1){
+    output1 += `${key}:${card1[key]}<br/><br/>`;
+}
+student1.innerHTML=output1;
+
+
+var student2 = document.querySelector(".student2");
+let card2 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output2 ="";
+for(let key in card2){
+    output2 += `${key}:${card2[key]}<br/><br/>`;
+}
+student2.innerHTML=output2;
+
+
+var student3 = document.querySelector(".student3");
+let card3 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output3 ="";
+for(let key in card3){
+    output3 += `${key}:${card3[key]}<br/><br/>`;
+}
+student3.innerHTML=output3;
+
+var student4 = document.querySelector(".student4");
+let card4 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output4 ="";
+for(let key in card4){
+    output4 += `${key}:${card4[key]}<br/><br/>`;
+}
+student4.innerHTML=output4;
+
+var student5 = document.querySelector(".student5");
+let card5 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output5 ="";
+for(let key in card5){
+    output5 += `${key}:${card5[key]}<br/><br/>`;
+}
+student5.innerHTML=output5;
+
+
+var student6 = document.querySelector(".student6");
+let card6 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output6 ="";
+for(let key in card6){
+    output6 += `${key}:${card6[key]}<br/><br/>`;
+}
+student6.innerHTML=output6;
+
+
+var student7 = document.querySelector(".student7");
+let card7 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output7 ="";
+for(let key in card7){
+    output7 += `${key}:${card7[key]}<br/><br/>`;
+}
+student7.innerHTML=output7;
+
+
+
+var student8 = document.querySelector(".student8");
+let card8 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output8 ="";
+for(let key in card8){
+    output8 += `${key}:${card8[key]}<br/><br/>`;
+}
+student8.innerHTML=output8;
+
+
+
+
+var student9 = document.querySelector(".student9");
+let card9 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output9 ="";
+for(let key in card9){
+    output9 += `${key}:${card9[key]}<br/><br/>`;
+}
+student9.innerHTML=output9;
+
+
+
+var student5 = document.querySelector(".student5");
+let card5 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output5 ="";
+for(let key in card5){
+    output5 += `${key}:${card5[key]}<br/><br/>`;
+}
+student5.innerHTML=output5;
+
+
+var student5 = document.querySelector(".student5");
+let card5 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output5 ="";
+for(let key in card5){
+    output5 += `${key}:${card5[key]}<br/><br/>`;
+}
+student5.innerHTML=output5;
+
+
+
+var student5 = document.querySelector(".student5");
+let card5 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output5 ="";
+for(let key in card5){
+    output5 += `${key}:${card5[key]}<br/><br/>`;
+}
+student5.innerHTML=output5;
+
+
+
+var student5 = document.querySelector(".student5");
+let card5 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output5 ="";
+for(let key in card5){
+    output5 += `${key}:${card5[key]}<br/><br/>`;
+}
+student5.innerHTML=output5;
+
+
+
+var student5 = document.querySelector(".student5");
+let card5 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output5 ="";
+for(let key in card5){
+    output5 += `${key}:${card5[key]}<br/><br/>`;
+}
+student5.innerHTML=output5;
+
+
+
+var student5 = document.querySelector(".student5");
+let card5 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output5 ="";
+for(let key in card5){
+    output5 += `${key}:${card5[key]}<br/><br/>`;
+}
+student5.innerHTML=output5;
+
+
+
+var student5 = document.querySelector(".student5");
+let card5 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output5 ="";
+for(let key in card5){
+    output5 += `${key}:${card5[key]}<br/><br/>`;
+}
+student5.innerHTML=output5;
+
+
+
+var student5 = document.querySelector(".student5");
+let card5 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output5 ="";
+for(let key in card5){
+    output5 += `${key}:${card5[key]}<br/><br/>`;
+}
+student5.innerHTML=output5;
+
+
+var student5 = document.querySelector(".student5");
+let card5 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output5 ="";
+for(let key in card5){
+    output5 += `${key}:${card5[key]}<br/><br/>`;
+}
+student5.innerHTML=output5;
+
+
+
+var student5 = document.querySelector(".student5");
+let card5 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output5 ="";
+for(let key in card5){
+    output5 += `${key}:${card5[key]}<br/><br/>`;
+}
+student5.innerHTML=output5;
+
+
+
+var student5 = document.querySelector(".student5");
+let card5 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output5 ="";
+for(let key in card5){
+    output5 += `${key}:${card5[key]}<br/><br/>`;
+}
+student5.innerHTML=output5;
+
+
+
+var student5 = document.querySelector(".student5");
+let card5 = {
+    student_name: "prince",
+    student_class: "12",
+    student_add : "surat",
+    student_id: "24",
+    student_birthdate: "14/12/2006",
+    student_blood_group: "B+",
+    student_mo : "+919988774455",
+    student_issue_date: "1/1/2008",
+    student_expired_date: " 1/1/2026",
+}
+let output5 ="";
+for(let key in card5){
+    output5 += `${key}:${card5[key]}<br/><br/>`;
+}
+student5.innerHTML=output5;
+
